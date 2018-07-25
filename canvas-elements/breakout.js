4f50cad76e4e5d934bd44cf9b620dcff375bf430
Daniel Moisoff <dmoisoff@gmail.com>
2018-07-25 18:34:47 -0400

    Background render with ground

---
+++ canvas-elements/breakout.js
@@ -0,0 +1,30 @@
+const canvas = document.getElementsByTagName('canvas')[0];
+const ctx = canvas.getContext("2d");
+let x = canvas.width/2;
+let y = canvas.height-30;
+let dx = 1;
+let dy = -1;
+const ballRadius = 10;
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+    dx = -dx;
+    }
+    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
+    x += dx;
+    y += dy;
+}
+
+setInterval(draw, 10);
--- canvas-elements/game.js
+++ canvas-elements/game.js
@@ -5,41 +5,71 @@ class Game{
     this._width = width;
     this._height = height;
     this._ctx = canvas.getContext('2d');
-    this.drawBall = this.drawBall.bind(this);
+    this.__clear = this._clear.bind(this);
+    // this.drawBall = this.drawBall.bind(this);
   }
 
-  // play(){
+
+  //   this._ctx.clearRect(0, 0, innerWidth, innerHeight);
+  //   this._ctx.beginPath();
+  //   this._ctx.arc(xPos, yPos, radius, 0, Math.PI * 2, false);
+  //   this._ctx.strokeStyle = 'blue';
+  //   this._ctx.stroke();
   //
-  //   requestAnimationFrame(this.play.bind(this));
-  // }
-
-  drawBall(){
-    this._ctx.beginPath();
-    this._ctx.arc(50, 50, 10, 0, Math.PI*2);
-    this._ctx.fillStyle = "#0095DD";
-    this._ctx.fill();
-    this._ctx.closePath();
-  }
-
-  draw(){
-    let x = this._width/2;
-    let y = this._height-30;
-    let dx = 2;
-    let dy = -2;
-
-    this.drawBall;
-    x += dx;
-    y += dy;
-  }
+  //   if (xPos + radius > innerWidth || xPos - radius < 0) {
+  //     dx = -dx;
+  //   }
+  //
+  //   if (yPos + radius > innerHeight || yPos - radius < 0) {
+  //     dy = -dy;
+  //   }
+  //   xPos += dx;
+  //   yPos += dy;
+  // };
 
 
-  _background(){
+
+
+  play(){
+    this._clear;
     let img = new Image();
-    img.src = "images/Mount_Fuji_from_mount_tanjo.jpg";
+    img.src = "images/Mount_Fuji_from_mount_tanjo crop.jpg";
     img.onload = () => (
-      this._ctx.drawImage(img, 0, 0, 800, 500)
+      this._ctx.drawImage(img, 0, 0, 800, 400)
     );
+    this._ctx.fillStyle = 'rgba(146,98,57,1)';
+    this._ctx.fillRect(0, 400, 1000, 100);
+
+    // let xPos = Math.random() * innerWidth;
+    // let yPos = Math.random() * innerHeight;
+    // let dx = (Math.random() - 0.5) * 10;
+    // let dy = (Math.random() - 0.5) * 15;
+    // const radius = 30;
+    //
+    // this._ctx.beginPath();
+    // this._ctx.arc(xPos, yPos, radius, 0, Math.PI * 2, false);
+    // this._ctx.strokeStyle = 'blue';
+    // this._ctx.stroke();
+    //
+    // if (xPos + radius > innerWidth || xPos - radius < 0) {
+    //   dx = -dx;
+    // }
+    //
+    // if (yPos + radius > innerHeight || yPos - radius < 0) {
+    //   dy = -dy;
+    // }
+    // xPos += dx;
+    // yPos += dy;
+    requestAnimationFrame(this.play.bind(this));
   }
+
+  // _background(){
+  //   let img = new Image();
+  //   img.src = "images/Mount_Fuji_from_mount_tanjo.jpg";
+  //   img.onload = () => (
+  //     this._ctx.drawImage(img, 0, 0, 800, 500)
+  //   );
+  // }
 
   _clear(){
     this._ctx.clearRect(0, 0, this._width, this._height);
@@ -55,6 +85,15 @@ class Game{
 
 
 const game = new Game(document.getElementsByTagName('canvas')[0],800,500);
-game._background();
-// game.play();
-setInterval(game.draw, 10);
+// game._background();
+game.play();
+// setInterval(game.draw, 10);
+
+// class Ball extends Game{
+//   constructor(props){
+//     super(props);
+//     this._ctx = props._ctx;
+//     this._width = props._width;
+//     this._height = props._height;
+//   }
+// }

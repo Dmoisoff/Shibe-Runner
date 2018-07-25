6d88ce4fdb9265016f29af1baa5aad448d3ed62a
Daniel Moisoff <dmoisoff@gmail.com>
2018-07-25 10:23:14 -0400

    Able to draw boxes

--- canavs-resize/canvas.js
+++ canavs-resize/canvas.js
@@ -1,2 +1,12 @@
-let canavs = document.querySelector('canvas');
-console.log(canavs);
+let canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let c = canvas.getContext('2d');
+// fillRect(x ,y width, height)
+c.fillRect(100, 100, 100, 100);
+c.fillRect(300, 300, 100, 100);
+c.fillRect(300, 100, 100, 100);
+c.fillRect(100, 300, 100, 100);
+c.fillRect(200, 200, 100, 100);
+c.fillRect(200, 400, 100, 100);
